05c8c8eccab5c06cdb0f2b833ba7458473ebc622
chris5zk <wzk789wzk@gmail.com>
2022-07-03 22:24:37 +0800

    fix import: first try

--- my_yolact/eval.py
+++ my_yolact/eval.py
@@ -1,13 +1,14 @@
-from yolact_edge.data import COCODetection, YoutubeVIS, get_label_map, MEANS, COLORS
-from yolact_edge.data import cfg, set_cfg, set_dataset
-from yolact_edge.yolact import Yolact
-from yolact_edge.utils.augmentations import BaseTransform, BaseTransformVideo, FastBaseTransform, Resize
-from yolact_edge.utils.functions import MovingAverage, ProgressBar
-from yolact_edge.layers.box_utils import jaccard, center_size
-from yolact_edge.utils import timer
-from yolact_edge.utils.functions import SavePath
-from yolact_edge.layers.output_utils import postprocess, undo_image_transformation
-from yolact_edge.utils.tensorrt import convert_to_tensorrt
+# eval import
+from my_yolact.yolact_edge.data import COCODetection, YoutubeVIS, get_label_map, MEANS, COLORS
+from my_yolact.yolact_edge.data import cfg, set_cfg, set_dataset
+from my_yolact.yolact_edge.yolact import Yolact
+from my_yolact.yolact_edge.utils.augmentations import BaseTransform, BaseTransformVideo, FastBaseTransform, Resize
+from my_yolact.yolact_edge.utils.functions import MovingAverage, ProgressBar
+from my_yolact.yolact_edge.layers.box_utils import jaccard, center_size
+from my_yolact.yolact_edge.utils import timer
+from my_yolact.yolact_edge.utils.functions import SavePath
+from my_yolact.yolact_edge.layers.output_utils import postprocess, undo_image_transformation
+from my_yolact.yolact_edge.utils.tensorrt import convert_to_tensorrt
 
 import pycocotools
 
